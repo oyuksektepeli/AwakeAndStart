dd4b7ed683b95d494ea779b7c6e4c0af34762331
Onur <onuryuksektepeli@hotmail.com.tr>
2020-09-03 19:08:35 +0300

    Initial commit

---
+++ Assets/Scripts/AwakeAndStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeAndStart : MonoBehaviour
+{
+    // Awake is callded even if component is disabled
+    void Awake()
+    {
+        Debug.Log("Awake called.");
+    }
+
+    // Start is called before the first frame update
+    // if component enabled
+    void Start()
+    {
+        Debug.Log("Start called.");
+    }
+}
